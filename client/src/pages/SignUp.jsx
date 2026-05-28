8d2a41c9d087c497e1701da30c64e3da10223abe
Code <samirbhandari1231@gmail.com>
2026-05-28 17:28:27 +0545

    Update signup API endpoint to production URL

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ const SignUp = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/signup", {
+      const res = await fetch("https://notes-backend-abc.onrender.com/api/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
